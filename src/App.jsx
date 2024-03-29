920745bb9db7ee0655701a43181a2b9edb3cc94f
Ralph Etienne <ralphyetienne@yahoo.com>
2024-03-28 23:37:41 -0400

    Initial commit, installed threejs and libraries associated for 3d animation and motion functionalities..

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Canvas from "./canvas"
+import Customizer from "./pages/Customizer"
+import Home from "./pages/Home"
+
+function App() {
+
+  return (
+   <main className="app transiton-all ease-in">
+     <h1 className="head-text">ThreeJS</h1>
+   </main>
+  )
+}
+
+export default App
